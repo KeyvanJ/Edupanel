d22fd38b9e192d41af1357c21eafa70948a972f5
KeyvanJ <keyvan.kjz@gmail.com>
2021-03-07 01:57:21 +0330

    finishing signup

--- hoviyat/views.py
+++ hoviyat/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+
 def signupuser(request):
-	return render(request, 'hoviyat/signupuser.html')
+	if request.method == 'GET':
+
+		return render(request, 'hoviyat/signupuser.html', {'form':UserCreationForm})
+
+	else:
+		#Create User
+		if request.POST['password1'] == request.POST['password2']:
+			user = User.objects.create.user(request.POST['username'], password=request.POST['password1'])
+			user.save()
+		#else: passwords don't match
